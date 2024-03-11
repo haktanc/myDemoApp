c7d5bf6df09bac92eee439fdc2f863088d25e7fa
haktanc <gshaktan17337@gmail.com>
2024-03-11 23:27:31 +0300

    Getting ready for deployment to heroku

---
+++ .README.md
@@ -0,0 +1,2 @@
+it takes 2 strings and 2 integers arrays as paramaters
+then sums lenghts of the strings and all the integers in arrays given as paramaters
--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,5 +1,16 @@
 package com.mycompany.app;
 
+import static spark.Spark.get;
+import static spark.Spark.port;
+import static spark.Spark.post;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.template.mustache.MustacheTemplateEngine;
+
 
 
 /**
@@ -8,8 +19,8 @@ package com.mycompany.app;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    //public static void main( String[] args )
+    //{
         //Integer[] array1 = {1, 2, 3};
         //Integer[] array2 = {4, 5, 6};
         //String str1 = "Hello";
@@ -17,7 +28,58 @@ public class App
 //
         //int result = sum(str1, str2, array1, array2);
         //System.out.println("Result: " + result);
-    }
+    //}
+
+
+public static void main(String[] args) {
+        port(getHerokuAssignedPort());
+
+        get("/", (req, res) -> "Hello, World");
+
+        post("/compute", (req, res) -> {
+        String str1 = req.queryParams("str1");
+        String str2 = req.queryParams("str2");
+
+        String[] array1String = req.queryParamsValues("array1");
+        Integer[] array1 = null;
+        if (array1String != null) {
+        array1 = new Integer[array1String.length];
+        for (int i = 0; i < array1String.length; i++) {
+        array1[i] = Integer.parseInt(array1String[i]);
+        }
+        }
+
+        String[] array2String = req.queryParamsValues("array2");
+        Integer[] array2 = null;
+        if (array2String != null) {
+        array2 = new Integer[array2String.length];
+        for (int i = 0; i < array2String.length; i++) {
+        array2[i] = Integer.parseInt(array2String[i]);
+        }
+        }
+
+        int result = sum(str1, str2, array1, array2);
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("result", result);
+        return new ModelAndView(map, "compute.mustache");
+        }, new MustacheTemplateEngine());
+
+        get("/compute", (rq, rs) -> {
+        Map<String, Object> map = new HashMap<>();
+        map.put("result", "not computed yet!");
+        return new ModelAndView(map, "compute.mustache");
+        }, new MustacheTemplateEngine());
+        }
+
+static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+        return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; // return default port if heroku-port isn't set (i.e. on localhost)
+        }
+
 
 
     public static int sum(String str1, String str2, Integer[] array1, Integer[] array2) {
