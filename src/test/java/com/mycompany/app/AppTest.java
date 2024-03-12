ffe5f6241a6ed62b6e514e50155b2dbe0a66f294
haktanc <gshaktan17337@gmail.com>
2024-03-12 15:51:30 +0300

    AppTest errors fixed

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -22,25 +22,24 @@ public class AppTest
 
     public void testSum1() {
         // test case 1: Both arrays contain positive numbers
-        ArrayList<Integer> array1 = new ArrayList<>(Arrays.asList(1, 2, 3);
-        ArrayList<Integer> array2 = new ArrayList<>(Arrays.asList(4, 5, 6);
+        ArrayList<Integer> array1 = new ArrayList<>(Arrays.asList(1, 2, 3));
+        ArrayList<Integer> array2 = new ArrayList<>(Arrays.asList(4, 5, 6));
         int result1 = App.sum("Hello", "World", array1, array2);
         assertEquals(31, result1);
     }
 
     public void testSum2() {
-        ArrayList<Integer> array1 = new ArrayList<>(Arrays.asList(1, 2, 3);
-        ArrayList<Integer> array2 = new ArrayList<>(Arrays.asList(4, 5, 6);
+        ArrayList<Integer> array1 = new ArrayList<>(Arrays.asList(1, 2, 3));
+        ArrayList<Integer> array2 = new ArrayList<>(Arrays.asList(4, 5, 6));
         // test case 2: One array contains negative numbers
-        Integer[] array3 = {-1, -2, -3};
-        ArrayList<Integer> array3 = new ArrayList<>(Arrays.asList(-1, -2, -3);
+        ArrayList<Integer> array3 = new ArrayList<>(Arrays.asList(-1, -2, -3));
         int result2 = App.sum("Good", "Morning", array3, array2);
         assertEquals(20, result2);
     }
 
     public void testSum3() {
-        ArrayList<Integer> array1 = new ArrayList<>(Arrays.asList(1, 2, 3);
-        ArrayList<Integer> array2 = new ArrayList<>(Arrays.asList(4, 5, 6);
+        ArrayList<Integer> array1 = new ArrayList<>(Arrays.asList(1, 2, 3));
+        ArrayList<Integer> array2 = new ArrayList<>(Arrays.asList(4, 5, 6));
         // test case 3: One array is null
         int result3 = App.sum("Hi", "There", array1, null);
         assertEquals(13, result3);
@@ -58,7 +57,7 @@ public class AppTest
 
     public void testSum5(){
         // test case 5: One string is empty, one array is null
-        ArrayList<Integer> array7 = new ArrayList<>(Arrays.asList(1, 2);
+        ArrayList<Integer> array7 = new ArrayList<>(Arrays.asList(1, 2));
         int result5 = App.sum("", "Test", array7, null);
         assertEquals(7, result5);
     }
